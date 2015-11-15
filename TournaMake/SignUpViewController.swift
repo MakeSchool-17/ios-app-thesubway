2dccce6b880a32528693b01936582917f76ae723
Daniel Hoang <thesubway@gmail.com>
2015-11-14 20:43:34 -0800

    Add outlets and actions for signup

--- TournaMake/SignUpViewController.swift
+++ TournaMake/SignUpViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet var textFieldName: UITextField!
+    @IBOutlet var textFieldEmail: UITextField!
+    @IBOutlet var textFieldPassword: UITextField!
+    @IBOutlet var textFieldConfirmPass: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -17,6 +22,12 @@ class SignUpViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func signupPressed(sender: AnyObject) {
+        print("sign up")
+    }
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
     }
 
 }
