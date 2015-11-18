3d53db02af5d06f257463e75351f8c6a4821affe
Daniel Hoang <thesubway@gmail.com>
2015-11-17 21:14:41 -0800

    Add FormValidation helper class

---
+++ TournaMake/FormValidation.swift
@@ -0,0 +1,19 @@
+//
+//  FormValidation.swift
+//  TournaMake
+//
+//  Created by Dan Hoang on 11/17/15.
+//  Copyright © 2015 Dan Hoang. All rights reserved.
+//
+
+import UIKit
+
+class FormValidation {
+    class func hasNonWhiteSpaceChar(str : String) -> Bool {
+        let trimmedStr = str.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
+        if trimmedStr == "" {
+            return true
+        }
+        return false
+    }
+}
