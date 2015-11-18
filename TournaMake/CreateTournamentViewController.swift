687eead72ee9b981941d6ea0c980fe2438a5500b
Daniel Hoang <thesubway@gmail.com>
2015-11-17 17:03:22 -0800

    Set tournament type to Group Stage + Knockout

--- TournaMake/CreateTournamentViewController.swift
+++ TournaMake/CreateTournamentViewController.swift
@@ -14,12 +14,15 @@ class CreateTournamentViewController: UIViewController {
     @IBOutlet var textFieldTournamentName: UITextField!
     @IBOutlet var textViewEntrants: UITextView!
     @IBOutlet var labelTotalTeams: UILabel!
+    let groupStageKnockout = "Group Stage + Knock-out"
     override func viewDidLoad() {
         super.viewDidLoad()
         let typeButton = UIButton(frame: CGRect(x: 0, y: 0, width: typePicker.frame.width, height: typePicker.frame.height))
         self.typePicker.addSubview(typeButton)
+        typePicker.text = groupStageKnockout
         typeButton.addTarget(self, action: "typePressed:", forControlEvents: UIControlEvents.TouchUpInside)
         self.textViewEntrants.layer.borderWidth = 1
+        self.textFieldTournamentName.autocapitalizationType = UITextAutocapitalizationType.Words
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,7 +30,7 @@ class CreateTournamentViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     func typePressed(sender: UIButton) {
-        print("type pressed")
+//        let typeArr : [String] = [groupStageKnockout]
     }
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         self.view.endEditing(true)
