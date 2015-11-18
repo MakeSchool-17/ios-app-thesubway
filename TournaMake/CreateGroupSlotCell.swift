e92e476f7cebef234ad349fc6fe50eac5a6c6f78
Daniel Hoang <thesubway@gmail.com>
2015-11-18 14:56:32 -0800

    Add tableView, and custom cell for create group

---
+++ TournaMake/CreateGroupSlotCell.swift
@@ -0,0 +1,24 @@
+//
+//  CreateGroupSlotCell.swift
+//  TournaMake
+//
+//  Created by Dan Hoang on 11/18/15.
+//  Copyright © 2015 Dan Hoang. All rights reserved.
+//
+
+import UIKit
+
+class CreateGroupSlotCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- TournaMake/CreateGroupStageViewController.swift
+++ TournaMake/CreateGroupStageViewController.swift
@@ -8,16 +8,29 @@
 
 import UIKit
 
-class CreateGroupStageViewController: UIViewController {
+class CreateGroupStageViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     var tournamentData : NSDictionary!
+    @IBOutlet var tableViewGroups: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableViewGroups.dataSource = self
+        self.tableViewGroups.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func submitPressed(sender : AnyObject?) {
+        print("submit groups")
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("createGroupSlotCell") as! CreateGroupSlotCell
+        return cell
     }
 
 }
