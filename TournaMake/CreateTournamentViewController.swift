c9f815cd08b305fc73e91cfcc8bbcf129fe4cf27
Daniel Hoang <thesubway@gmail.com>
2015-11-17 16:55:20 -0800

    Set up UI for CreateTournament

--- TournaMake/CreateTournamentViewController.swift
+++ TournaMake/CreateTournamentViewController.swift
@@ -10,13 +10,30 @@ import UIKit
 
 class CreateTournamentViewController: UIViewController {
 
+    @IBOutlet var typePicker: UITextField!
+    @IBOutlet var textFieldTournamentName: UITextField!
+    @IBOutlet var textViewEntrants: UITextView!
+    @IBOutlet var labelTotalTeams: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let typeButton = UIButton(frame: CGRect(x: 0, y: 0, width: typePicker.frame.width, height: typePicker.frame.height))
+        self.typePicker.addSubview(typeButton)
+        typeButton.addTarget(self, action: "typePressed:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.textViewEntrants.layer.borderWidth = 1
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    func typePressed(sender: UIButton) {
+        print("type pressed")
+    }
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    @IBAction func submitPressed(sender: AnyObject) {
+        print("submit")
     }
 
 }
