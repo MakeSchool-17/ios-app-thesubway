31062cb7ea887202495685c605e4744b4c0779f5
Daniel Hoang <thesubway@gmail.com>
2015-12-08 14:25:25 -0800

    Add BracketViewController with stackView

---
+++ TournaMake/BracketViewController.swift
@@ -0,0 +1,23 @@
+//
+//  BracketViewController.swift
+//  TournaMake
+//
+//  Created by Dan Hoang on 12/8/15.
+//  Copyright © 2015 Dan Hoang. All rights reserved.
+//
+
+import UIKit
+
+class BracketViewController: UIViewController {
+
+    @IBOutlet var stackViewBracket: UIStackView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
