15ab5de94c479fd2445bd72980d7084c3c20a1be
Daniel Hoang <thesubway@gmail.com>
2016-08-01 20:50:16 -0700

    Place temp height for 16-entrant match 11

--- TournaMake/DoubleEliminationViewController.swift
+++ TournaMake/DoubleEliminationViewController.swift
@@ -372,6 +372,13 @@ class DoubleEliminationViewController: UIViewController, UITextFieldDelegate, UI
                 let imgVw = UIImageView(frame: CGRect(x: xVal, y: yVal, width: 40, height: 40))
                 imgVw.image = UIImage(named: "downArrow.png")
                 self.largeSubView.addSubview(imgVw)
+            }
+            else if i == 11 {
+                let upperView = self.largeSubView.viewWithTag(219)!
+                let xVal = upperView.center.x - 20
+                let imgVw = UIImageView(frame: CGRect(x: xVal, y: yVal, width: 40, height: 40))
+                imgVw.image = UIImage(named: "downArrow.png")
+                self.largeSubView.addSubview(imgVw)
             }*/
             self.largeSubView.addSubview(vw)
             currentY += matchHeight + verticalSpacing
